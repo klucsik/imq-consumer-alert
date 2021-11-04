39dd641778d14a122de69cc29ab27c08d9775c96
klucsik <krisztian.klucsik@pontsystems.eu>
2021-11-04 15:05:35 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+feladat:
+a script adjon vissza true-t hogyha valamelyik GW-RQ 70 felett van, vagy ha bármelyik másik 50 felett van consumberszám szerint
---
+++ magic.sh
@@ -0,0 +1,18 @@
+# extract table:  
+#       cat input-table-one-with51-gw-rw-69 | awk '/RUNNING/'
+
+#  extract table and print only the two interesting columns: 
+#       cat input-table-one-with51-gw-rw-69 | awk '/RUNNING/ {print $1, $6}'
+
+# print out only if consumers are over 50
+#       cat input-table-one-with51-gw-rw-69 | awk '/RUNNING/ && $6>50 {print $1, $6} '
+
+# print out only if consumers are over 50 and its not the GW_RQ
+#       cat input-table-one-with51-gw-rw-69 | awk '/RUNNING/ && $6>50 && !/GW_RQ/ {print $1, $6} '
+
+# print out only if consumers are over 70 and it is the GW_RQ
+#      cat input-table-gw-rw-over70 | awk '/RUNNING/ && $6>70 && /GW_RQ/ {print $1, $6} '
+
+#use if and else if to combine the two
+
+cat input-table-one-with51-gw-rw-69 | awk '{if(/RUNNING/ && !/GW_RQ/ && $6>50) print $1, $6; else if(/RUNNING/ && /GW_RQ/ && $6>70) print $1, $6;} '
